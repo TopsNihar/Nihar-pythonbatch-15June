70a6ba2feda984d2d75c39fb5eb28ba9f219a3a2
TopsNihar <niharnakrani199@gmail.com>
2024-06-22 18:41:58 +0530

    module-1 is done

---
+++ assignment/q1.c
@@ -0,0 +1,5 @@
+1]What is software? What is software engineering?
+->  software is a collection of programming.
+    Ex: ios,windows
+
+->  software engineering isis technique through which we can devlop or create software.
