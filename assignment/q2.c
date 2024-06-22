cb06a21e4e18c4b964a384d823f889e53836f573
TopsNihar <niharnakrani199@gmail.com>
2024-06-22 18:59:13 +0530

    module-1 is done

---
+++ assignment/q2.c
@@ -0,0 +1,11 @@
+//2]Explain types of software?
+//->1.system operater
+  	Operating System 
+  	Gadget Drivers
+  	Firmware
+  	Utility
+  //2.application software
+  	Word Processors 
+ 	Database Software
+ 	Sight and sound Software
+	Internet Browsers
