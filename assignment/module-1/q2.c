36664c561dc794f115921cf7d5496f4b41f61da4
TopsNihar <niharnakrani199@gmail.com>
2024-07-04 10:21:57 +0530

    Update q2.c

---
+++ assignment/module-1/q2.c
@@ -0,0 +1,22 @@
+/2]Explain types of software?
+//->1.system operater
+  	I]Operating System: It is the main program of a computer system. When the computer system ON it is the first software that loads
+	 into the computer’s memory. Basically, it manages all the resources such as computer memory, CPU, printer, hard disk, etc., and 
+	 provides an interface to the user, which helps the user to interact with the computer system. It also provides various services 
+	 to other computer software. Examples of operating systems are Linux, Apple macOS, Microsoft Windows, etc.
+    II]Language Processor: As we know that system software converts the human-readable language into a machine language and vice versa. 
+	 So, the conversion is done by the language processor. It converts programs written in high-level programming languages like Java, C,
+	 C++, Python, etc(known as source code), into sets of instructions that are easily readable by machines(known as object code or
+	 machine code).
+    III]Device Driver: A device driver is a program or software that controls a device and helps that device to perform its functions.
+	 Every device like a printer, mouse, modem, etc. needs a driver to connect with the computer system eternally. So, when you connect
+	 a new device with your computer system, first you need to install the driver of that device so that your operating system knows
+	 how to control or manage that device.
+  //2.application software
+  	I]General Purpose Software: This type of application software is used for a variety of tasks and it is not limited to performing a 
+	 specific task only. For example, MS-Word, MS-Excel, PowerPoint, etc.
+    II]Customized Software: This type of application software is used or designed to perform specific tasks or functions or designed 
+	 for specific organizations. For example, railway reservation system, airline reservation system, invoice management system, etc.
+    III]Utility Software: This type of application software is used to support the computer infrastructure. It is designed to analyze,
+	 configure, optimize and maintains the system, and take care of its requirements as well. For example, antivirus, disk fragmenter, 
+	 memory tester, disk repair, disk cleaners, registry cleaners, disk space analyzer, etc.
